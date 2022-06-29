1b290a23ce22e850a36928a7a5f02b63bdec2c4b
Anton udén <udenanton@hotmail.com>
2022-06-29 15:19:29 +0200

    Set announceAdvancements to false on start

--- src/net/novauniverse/games/survivalgames/game/SurvivalGames.java
+++ src/net/novauniverse/games/survivalgames/game/SurvivalGames.java
@@ -415,6 +415,8 @@ public class SurvivalGames extends MapGame implements Listener {
 				}
 			}.runTaskLater(getPlugin(), 20L);
 		}
+
+		getWorld().setGameRuleValue("announceAdvancements", "false");
 	}
 
 	public boolean startCountdown() {
